307ddfc7e68557f712f822693732d57e7d7f0508
NetDevs <ryan@netdevs.net>
2024-12-08 17:15:14 -0800

    gitignore	added *dist/

--- random-map/src/index.ts
+++ random-map/src/index.ts
@@ -1,14 +1,15 @@
 /// <reference types="@types/google.maps" />
 
+// importing Classes
 import { User } from './User';
 import { Company } from './Company';
 import { CustomMap } from './CustomMap';
 
-
+// building the map 
 (window as any).initMap = () => {
 	const user = new User();
 	const company = new Company();
 	const customMap = new CustomMap('map');
-	customMap.addUserMarker(user);
-	customMap.addCompanyMarker(company);
+	customMap.addMarker(user);
+	customMap.addMarker(company);
 };
